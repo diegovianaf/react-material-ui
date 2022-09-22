a3f062335de960741699527c9eb4d2d11e622388
Diego Viana <diegovianaf7@gmail.com>
2022-09-22 13:51:22 -0300

    add material theme

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,26 @@
+import { createTheme, ThemeProvider } from '@mui/material'
+import { deepPurple, indigo } from '@mui/material/colors'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
 import './index.css'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: indigo[500],
+    },
+    secondary: {
+      main: deepPurple[900],
+    },
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
