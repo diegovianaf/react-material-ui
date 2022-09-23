695c5f0b72359c91d2466a5f56c79aad58bf635f
Diego Viana <diegovianaf7@gmail.com>
2022-09-23 11:55:27 -0300

    remove unused import

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,4 +1,4 @@
-import { Container, Grid } from '@mui/material'
+import { Grid } from '@mui/material'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import CustomerCard from '../components/CustomerCard'
