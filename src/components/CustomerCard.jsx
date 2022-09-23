ec93d171dccad027276cbff83d782a794b84d7f1
Diego Viana <diegovianaf7@gmail.com>
2022-09-23 13:32:42 -0300

    add modal to confirm user actions

--- src/components/CustomerCard.jsx
+++ src/components/CustomerCard.jsx
@@ -1,6 +1,8 @@
 import { Avatar, Card, CardActions, CardHeader, IconButton } from '@mui/material'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import ShareIcon from '@mui/icons-material/Share'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import ModalConfirm from './ModalConfirm'
+import { useState } from 'react'
 
 const CustomerCard = ({
   firstName,
@@ -8,26 +10,52 @@ const CustomerCard = ({
   email,
   avatar,
 }) => {
+  const [openModal, setOpenModal] = useState(false)
+
+  const handleToggleModalConfirm = () => {
+    setOpenModal(!openModal)
+  }
+
+  const handleConfirmModal = () => {
+    alert('ok')
+  }
+
+  const handleDeleteCustomer = () => {
+    handleToggleModalConfirm()
+  }
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="customer" src={avatar}>
-            R
-          </Avatar>
-        }
-        title={`${firstName} ${lastName}`}
-        subheader={email}
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="customer" src={avatar}>
+              R
+            </Avatar>
+          }
+          title={`${firstName} ${lastName}`}
+          subheader={email}
+        />
+        <CardActions disableSpacing>
+          <IconButton aria-label="edit user register">
+            <EditIcon />
+          </IconButton>
+          <IconButton 
+            aria-label="delete register"
+            onClick={handleDeleteCustomer}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+      <ModalConfirm
+        open={openModal}
+        onClose={handleToggleModalConfirm}
+        onConfirm={handleConfirmModal}
+        title="Are you sure you want to delete it?"
+        message="By confirming it is not possible to reverse this action"
       />
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
+    </>
   )
 }
 
